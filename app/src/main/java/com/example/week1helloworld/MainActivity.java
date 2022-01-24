4a9d894dac5e25c405d7eb17a69c5028d7ee6ef9
rania arbash <raniaarbash@hotmail.com>
2022-01-24 16:00:26 -0500

    Adding OnClickListener

--- app/src/main/java/com/example/week1helloworld/MainActivity.java
+++ app/src/main/java/com/example/week1helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,13 +13,17 @@ import android.widget.Toast;
 
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
     EditText usernameEditText;
     EditText userIDEditText;
 
     ImageView welcomeImage;
     TextView dateText;
     int value = 0 ;
+    Button signup_btn;
+    Button help_btn;
+    Button help2_btn;
 
     Student s1 ;
     //R.java
@@ -36,6 +41,26 @@ public class MainActivity extends AppCompatActivity {
         welcomeImage.setImageResource(R.drawable.img2);
         dateText.setText((new Date().toString()));
         Log.d("week 1","on create " + value);
+
+
+        // second scnario
+        signup_btn = findViewById(R.id.signup);
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("week 1", "set click listener for a button");
+                // do whatever I want here.
+                Toast.makeText(getApplicationContext(),"Sign Up Clicked !!! ",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // third scenario
+        help_btn = findViewById(R.id.help);
+        help_btn.setOnClickListener(this);
+
+        help2_btn = findViewById(R.id.help2);
+        help2_btn.setOnClickListener(this);
+
     }
 
     @Override
@@ -75,13 +100,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void logInClicked(View view) {
-        Log.d("week 1", "Log in clicked  " );
+        Log.d("week 1", "Add a public void function with View parameter and give its name to the layout");
+        Toast.makeText(getApplicationContext()," Sign In Clicked !!! ",Toast.LENGTH_LONG).show();
+
 
         if (usernameEditText.getText().toString().isEmpty() || (userIDEditText.getText().toString().isEmpty())) {
             Toast.makeText(this,"ERROE !!!  Not Enough Information !!!",Toast.LENGTH_LONG).show();
 
         }
-        if (userIDEditText.getText().toString().length() > 8){
+        else if (userIDEditText.getText().toString().length() > 8){
             Toast.makeText(this,"ERROE !!!  Very long ID !!!",Toast.LENGTH_LONG).show();
 
         }
@@ -95,5 +122,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.help || id == R.id.help2){
+            Log.d("week 1", "Implement OnClick Listener on the activity level");
+            // do whatever I want here.
+            Toast.makeText(getApplicationContext()," Help Clicked !!! ",Toast.LENGTH_LONG).show();
+        }
     }
 }
